77ab38fb893144a4a34383b6493bf4b8ce558902
John Doe <max.s32@i.ua>
2016-04-04 21:02:08 +0300

    Initial client's encryption/decryption implementation (Anglular).

--- app/assets/javascripts/crypt.js
+++ app/assets/javascripts/crypt.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+angular.module('crypt', [])
+.controller('MainCtrl', ['$http', function($http) {
+  var self = this;
+  self.encrypt = function() {
+    $http.post('/encrypt',
+               {'crypt': {'text': self.plain, 'shift':self.rot}})
+      .then(function(response) {
+        self.encrypted = response.data.encrypted;
+        self.plain = '';
+      });
+  };
+  self.decrypt = function() {
+    $http.post('/decrypt',
+               {'crypt': {'text': self.encrypted, 'shift':self.rot}})
+      .then(function(response) {
+        self.plain = response.data.plain;
+        self.encrypted = '';
+      });
+  };
+}])
+.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+}]);
