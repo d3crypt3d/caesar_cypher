956050160b542146209c18a312a25cf17c7dc016
John Doe <max.s32@i.ua>
2016-04-05 15:52:28 +0300

    Crypt controller now returns frequency analysis data. Fix a bug when the controller recieves non-latin chars. Minor changes in welcome layout.

--- app/controllers/crypt_controller.rb
+++ app/controllers/crypt_controller.rb
@@ -3,29 +3,36 @@ class CryptController < ApplicationController
 
   def encrypt
     output= @text.split('').inject('') do |memo, char|
-      ind = @alphabet.index(char)
+      ind = @alphabet.index(char) || 0 # If someone uses another but english layout
       memo + @rot_alphabet[ind]
     end
 
     respond_to do |format|
-      format.json { render json: {plain: @text, encrypted: output}, status: :ok }
+      format.json { render json: {plain: @text, encrypted: output, frequency: frequency_analisys(output)}, status: :ok }
     end
   end
  
   def decrypt
     output= @text.split('').inject('') do |memo, char|
-      ind = @rot_alphabet.index(char)
+      ind = @rot_alphabet.index(char) || 0
       memo + @alphabet[ind]
     end  
 
     respond_to do |format|
-      format.json { render json: {plain: output, encrypted: @text}, status: :ok }
+      format.json { render json: {plain: output, encrypted: @text, frequency: frequency_analisys(output)}, status: :ok }
     end
   end
 
   private
+  def frequency_analisys(input_text)
+    input_text.split('').group_by{|x| x}.inject([]) do |memo, i|
+      memo << [i[0], i[1].length]
+    end
+  end
+
   def set_crypt_vars
     @text, shifted_range = crypt_params.values_at(:text, :shift)
+    @text ||= ''
     @alphabet = Text.english_alphabet
     @alph_length = @alphabet.length
     @rot_alphabet = @alphabet.rotate(shifted_range.to_i)
